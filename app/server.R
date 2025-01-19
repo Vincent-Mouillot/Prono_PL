a754a3916285070fd0dac9d23b30fb935b24ad52
Vincent Mouillot <vmouillot@gmail.com>
2025-01-19 17:37:57 +0100

    Start to build app with display history

---
+++ app/server.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(DT)
+library(DBI)
+library(RSQLite)
+library(rprojroot)
+
+root <<- rprojroot::find_root(rprojroot::has_dir("Prono_PL"))
+
+sqlite_file <- file.path(root, "Prono_PL", "my_database.db")
+
+con <- dbConnect(RSQLite::SQLite(), dbname = sqlite_file)
+
+df_team <- dbGetQuery(con, "SELECT * FROM Table_teams
+                      WHERE Comp = 'Premier League';")
+
+df_prono <- dbGetQuery(
+  con,
+  "SELECT
+      h.Date,
+      t1.Other_Names AS Home_Team_Name,
+      t2.Other_Names AS Away_Team_Name,
+      h.H_percent,
+      h.D_percent,
+      h.A_percent,
+      h.score_pred,
+      h.score_pred_percent,
+      c.score_home,
+      c.score_away,
+      c.result
+   FROM Prono_history AS h
+   JOIN Table_teams AS t1 ON h.H_team = t1.Id
+   JOIN Table_teams AS t2 ON h.A_team = t2.Id
+   JOIN Calendrier AS c ON c.Home_id = h.H_team
+                        AND c.Away_id = h.A_team;"
+)
+
+dbDisconnect(con)
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+
+  output$history_table <- renderDataTable({
+    datatable(
+      df_prono %>% 
+        filter(!is.na(result)) %>% 
+        arrange(desc(Date)),
+      options = list(
+        pageLength = 15,
+        dom = 'frtip',
+        autoWidth = TRUE
+      ),
+      rownames = FALSE,
+      class = "stripe hover"
+    )
+  })
+  
+  output$next_game_table <- renderDataTable({
+    datatable(
+      df_prono %>% 
+        filter(is.na(result),
+               ymd(Date) - today() == 0) %>% 
+        arrange(desc(Date)),
+      options = list(
+        pageLength = 10,
+        dom = '',
+        autoWidth = TRUE
+      ),
+      rownames = FALSE,
+      class = "stripe hover"
+    )
+  })
+  
+}
---
+++ app/ui.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(shinydashboard)
+dashboardPage(
+  dashboardHeader(title = "Pronos PL"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Next games", tabName = "next_game", icon = icon("chart-line")),
+      menuItem("Performance of the model", 
+               tabName = "perf_model", icon = icon("chart-line")),
+      menuItem("Prono history", tabName = "history", icon = icon("chart-line"))
+    )
+  ),
+  dashboardBody(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+    ),
+    tabItems(
+      tabItem(tabName = "home", h2("Bienvenue dans l'application!")),
+      tabItem(tabName = "next_game",
+              dataTableOutput("next_game_table")),
+      tabItem(tabName = "perf_model", h2("Brier Score!")),
+      tabItem(tabName = "history", h2("Table of last prono"),
+              dataTableOutput("history_table"))
+    )
+  )
+)
