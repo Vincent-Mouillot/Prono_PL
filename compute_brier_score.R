0b5caa8e2766e3cb35e1b3f6b4474b6845c62df4
Vincent Mouillot <vmouillot@gmail.com>
2025-01-22 18:00:53 +0100

    Make the notebook into a usable function

--- compute_brier_score.R
+++ compute_brier_score.R
@@ -1,56 +1,11 @@
-library(DBI)
-library(RSQLite)
-library(tidyverse)
-library(rprojroot)
 library(data.table)
 
-root <- rprojroot::find_root(rprojroot::has_dir("Prono_PL"))
-
-sqlite_file <- file.path(root, "Prono_PL", "my_database.db")
-
-con <- dbConnect(RSQLite::SQLite(), dbname = sqlite_file)
-
-df_compl <- dbGetQuery(
-  con,
-  "SELECT p.H_team,
-          p.A_team,
-          p.H_percent,
-          p.D_percent,
-          p.A_percent,
-          r.result
-  FROM Calendrier AS r
-     JOIN Prono_history AS p ON p.H_team = r.Home_id 
-                                  AND  p.A_team = r.Away_id
-  WHERE r.result IS NOT NULL;"
-)
-
-df_opta <- dbGetQuery(
-  con,
-  "SELECT p.H_team,
-          p.A_team,
-          p.H_percent,
-          p.D_percent,
-          p.A_percent,
-          r.result
-  FROM Calendrier AS r
-     JOIN Opta AS p ON p.H_team = r.Home_id 
-                                  AND  p.A_team = r.Away_id
-  WHERE r.result IS NOT NULL;"
-)
-
-dbDisconnect(con)
-
-one_hot <- as.data.table(model.matrix(~ result - 1, data = df_compl))
-setnames(one_hot, old = colnames(one_hot), new = gsub("result", "result_", colnames(one_hot)))
-df_compl <- cbind(df_compl, one_hot) %>% 
-  select(-result)
-
-one_hot_op <- as.data.table(model.matrix(~ result - 1, data = df_opta))
-setnames(one_hot_op, old = colnames(one_hot_op), new = gsub("result", "result_", colnames(one_hot_op)))
-df_opta <- cbind(df_opta, one_hot_op) %>% 
-  select(-result)
-
 brier_score <- function(df, mean=FALSE){
+  one_hot <- as.data.table(model.matrix(~ result - 1, data = df))
+  setnames(one_hot, old = colnames(one_hot), new = gsub("result", "result_", colnames(one_hot)))
+  df <- cbind(df, one_hot) %>% 
+    select(-result)
+  
   df <- df %>% 
     mutate(H_percent = H_percent/100,
            D_percent = D_percent/100,
@@ -65,6 +20,3 @@ brier_score <- function(df, mean=FALSE){
   
   return(df)
 }
-
-brier_score(df_compl, mean=TRUE)
-brier_score(df_opta, mean=TRUE)
