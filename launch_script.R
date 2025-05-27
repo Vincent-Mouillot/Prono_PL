2c7d011d612aeec91f238b8df8bb247fe58f7f4e
Vincent Mouillot <vmouillot@gmail.com>
2025-05-27 14:25:46 +0200

    Add update l1 db

--- launch_script.R
+++ launch_script.R
@@ -5,15 +5,17 @@ library(rprojroot)
 
 root <<- rprojroot::find_root(rprojroot::has_dir("Prono_PL"))
 
-source(file.path(root, "Prono_PL", "get_cloud_db.R"))
-
 source(file.path(root, "Prono_PL", "get_cloud_db_l1.R"))
 
 source(file.path(root, "Prono_PL", "get_calendrier_l1.R"))
 
 source(file.path(root, "Prono_PL", "get_all_data_l1.R"))
 
+drive_update(file, media = temp_db_path)
+
 #script pour telecharger la table et enregistrer une table temp
+
+source(file.path(root, "Prono_PL", "get_cloud_db.R"))
 
 source(file.path(root, "Prono_PL", "get_calendrier.R"))
 
