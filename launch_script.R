a5df01ecb90ac28548aff61bdf14a5ac02817865
Vincent <vmouillot@gmail.com>
2025-07-30 13:32:27 +0200

    Get the init db after downloading it if exist

--- launch_script.R
+++ launch_script.R
@@ -5,19 +5,11 @@ library(rprojroot)
 
 root <<- rprojroot::find_root(rprojroot::has_dir("Prono_PL"))
 
-source(file.path(root, "Prono_PL", "get_cloud_db_l1.R"))
-
-source(file.path(root, "Prono_PL", "get_calendrier_l1.R"))
-
-source(file.path(root, "Prono_PL", "get_all_data_l1.R"))
-
-drive_update(file, media = temp_db_path)
-
 #script pour telecharger la table et enregistrer une table temp
 
-source(file.path(root, "Prono_PL", "init_db.R"))
-
 source(file.path(root, "Prono_PL", "get_cloud_db.R"))
+
+source(file.path(root, "Prono_PL", "init_db.R"))
 
 source(file.path(root, "Prono_PL", "get_calendrier.R"))
 
