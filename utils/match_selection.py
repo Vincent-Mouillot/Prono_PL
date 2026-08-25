7085c6f63a4cc22a68fa83bee6c33b67f46da5e8
Vincent Mouillot <vmouillot@gmail.com>
2026-08-25 17:08:52 +0200

    Add inferior date to match selection

--- utils/match_selection.py
+++ utils/match_selection.py
@@ -7,4 +7,5 @@ def match_selection(df: pd.DataFrame, nb_days: int = 0):
 
     df_coming = df.loc[df["xg"].isnull()].sort_values("datetime").reset_index(drop=True)
     today = pd.Timestamp(date.today())
-    return df_coming.loc[pd.to_datetime(df_coming["datetime"]).dt.date <= (today.date() + timedelta(days=nb_days))]
+    return df_coming.loc[(pd.to_datetime(df_coming["datetime"]).dt.date <= (today.date() + timedelta(days=nb_days)))
+                         & (pd.to_datetime(df_coming["datetime"]).dt.date >= today.date())]
